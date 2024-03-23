f9b52db0ac4be7ee9d2598f785e494d5dbeae427
akshat <akshatbhatya@gmail.com>
2024-03-23 09:33:10 +0530

    create server using express

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
-import { Express } from "express";
+import  Express  from "express";
 
+
+
+const port=8080;
 const app=Express();
 
-app.listen.get('/',(req,res)=>{
+
+app.get('/',(req,res)=>{
     res.send("<h2>server is started</h2>")
 })
-
+app.get('/about',(req,res)=>{
+    res.send('about')
+})
+app.listen(port,()=>{
+    console.log("http://localhost:8080","server is started");
+})
