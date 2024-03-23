445a9f24a57b5dee772877e4710ab5af005cc70b
akshat <akshatbhatya@gmail.com>
2024-03-23 13:09:18 +0530

    updated

--- frontend/crete front end for application/src/App.jsx
+++ frontend/crete front end for application/src/App.jsx
@@ -1,7 +1,30 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 function App() {
+
+  const [user,setuser]=useState([])
+
+  useEffect(()=>{
+    axios.get("http://localhost:8080")
+    .then((response)=>setuser(response.data))
+    .catch((err)=>{console.log(err)})
+  },[])
+
+  if(user.length===0){
+    return;
+  }
+
   return (
     <>
-      <h2>hi</h2>
+      <h2>{user.length}</h2>
+
+      {user.map((user)=>{
+        return <div key={user.id}>
+          <h5>TITLE : {user.title}</h5>
+          <h6>body : {user.body}</h6>
+
+        </div>
+      })}
     </>
   )
 }
